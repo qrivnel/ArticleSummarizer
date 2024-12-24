a7942e2d2b41c8461c01bd885c3105c46f2fa4dd
Erhan <uaerhan.ek@gmail.com>
2024-12-24 14:41:13 +0300

    firestore was added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import {
   Box,
-  Container,
+  Grid,
   TextField,
   Button,
   Typography,
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import ChatRow from "./components/chat-row";
 import axios from "axios";
+import firestore from "../firebase";
 
 const theme = createTheme({
   palette: {
@@ -29,15 +30,23 @@ const theme = createTheme({
 });
 
 function App() {
-  const [message, setMessage] = useState<string>();
+  const [message, setMessage] = useState<string>("");
   const [selectedChat, setSelectedChat] = useState<any>();
   const [chats, setChats] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
+  const getChats = async () => {
     setChats([])
-  }, [])
-  
+    const data = await firestore.collection("chats").get();
+    const chatData = data.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setChats(chatData);
+  };
+  useEffect(() => {
+    getChats();
+  }, []);
 
   const handleChange = (event: any) => {
     setMessage(event.target.value);
@@ -48,171 +57,193 @@ function App() {
   };
 
   const getSummary = async () => {
-    if(message)
-      return await axios.post("http://127.0.0.1:5000/summarize", {article: message.trim()})
-  }
+    if (message)
+      return await axios.post("http://127.0.0.1:5000/summarize", {
+        article: message.trim(),
+      });
+  };
 
   const sendMessage = async () => {
     setLoading(true);
-    setSelectedChat({article: message})
-    setMessage("")
+    setSelectedChat({ article: message });
+    setMessage("");
     try {
-      const res: any = await getSummary()
-      const {title, summary} = res.data
+      const res: any = await getSummary();
+      const { title, summary } = res.data;
       setSelectedChat((prev: any) => ({
         ...prev,
         title: title,
-        summary: summary
-      }))
+        summary: summary,
+      }));
+      if (res.data.summary) {
+        await firestore
+          .collection("chats")
+          .add({
+            title: title,
+            summary: summary,
+            article: message,
+          })
+          .then((res: any) => {
+            getChats()
+          });
+      }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  
+
     setLoading(false);
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{display: "flex", flexDirection: "row", width: "100vw", height: "100vh", alignItems: "center"}}>
-        <Container
-          maxWidth="sm"
-          sx={{ paddingTop: 2, display: "flex", flexDirection: "column", width: "50%", height: "100%" }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              height: "100%",
-              backgroundColor: "background.default",
-              borderRadius: 1,
-              boxShadow: 3,
-            }}
-          >
-            <Box
-              sx={{
-                padding: 2,
-                borderBottom: 1,
-                borderColor: "divider",
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <Typography variant="h5" color="white">
-                Makale Özetleyici
-              </Typography>
-            </Box>
-  
-            {/* Sohbet Mesajları */}
-            {!selectedChat ? (
-              <List sx={{ overflowY: "auto", flexGrow: 1, padding: 2 }}>
-                <ListItem>
-                  <Paper
-                    sx={{ padding: 1, backgroundColor: "#333", color: "white" }}
-                  >
-                    Özetini çıkarmak istediğiniz makaleyi gönderebilirsiniz.
-                  </Paper>
-                </ListItem>
-              </List>
-            ) : (
-              <List sx={{ overflowY: "auto", flexGrow: 1, padding: 2 }}>
-                <ListItem sx={{ justifyContent: "end" }}>
-                  <Paper
-                    sx={{
-                      padding: 1,
-                      backgroundColor: "primary.main",
-                      color: "white",
-                      width: "70%",
-                    }}
-                  >
-                    {selectedChat.article}
-                  </Paper>
-                </ListItem>
-                <ListItem>
-                  <Paper
-                    sx={{
-                      padding: 1,
-                      backgroundColor: "#333",
-                      color: "white",
-                      width: "70%",
-                    }}
-                  >
-                    {loading ?  <CircularProgress /> : selectedChat.summary}
-                  </Paper>
-                </ListItem>
-              </List>
-            )}
+      <Box sx={{ flexGrow: 1, width: "100vw", height: "100vh", padding: 2 }}>
+        <Grid container spacing={2} sx={{ height: "100%" }}>
+          {/* Chat Input and Messages Section */}
+          <Grid item xs={12} md={8} sx={{ height: "100%" }}>
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "column",
-                padding: 2,
-                borderTop: 1,
-                borderColor: "divider",
+                height: "100%",
+                backgroundColor: "background.default",
+                borderRadius: 1,
+                boxShadow: 3,
               }}
             >
-              {!selectedChat?.summary ? (
-                <>
-                  <TextField
-                    label="Mesajınızı yazın"
-                    variant="outlined"
-                    value={message}
-                    onChange={handleChange}
-                    multiline
-                    minRows={1}
-                    maxRows={5}
-                    fullWidth
-                    sx={{
-                      "& .MuiInputBase-root": {
-                        minHeight: "40px",
-                      },
-                    }}
-                  />
+              <Box
+                sx={{
+                  padding: 2,
+                  borderBottom: 1,
+                  borderColor: "divider",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography variant="h5" color="white">
+                  Makale Özetleyici
+                </Typography>
+              </Box>
+
+              {/* Message Display */}
+              <List sx={{ overflowY: "auto", flexGrow: 1, padding: 2 }}>
+                {!selectedChat ? (
+                  <ListItem>
+                    <Paper
+                      sx={{
+                        padding: 1,
+                        backgroundColor: "#333",
+                        color: "white",
+                      }}
+                    >
+                      Özetini çıkarmak istediğiniz makaleyi gönderebilirsiniz.
+                    </Paper>
+                  </ListItem>
+                ) : (
+                  <>
+                    <ListItem sx={{ justifyContent: "end" }}>
+                      <Paper
+                        sx={{
+                          padding: 1,
+                          backgroundColor: "primary.main",
+                          color: "white",
+                          width: "70%",
+                        }}
+                      >
+                        {selectedChat.article}
+                      </Paper>
+                    </ListItem>
+                    <ListItem>
+                      <Paper
+                        sx={{
+                          padding: 1,
+                          backgroundColor: "#333",
+                          color: "white",
+                          width: "70%",
+                        }}
+                      >
+                        {loading ? <CircularProgress /> : selectedChat.summary}
+                      </Paper>
+                    </ListItem>
+                  </>
+                )}
+              </List>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: 2,
+                  borderTop: 1,
+                  borderColor: "divider",
+                }}
+              >
+                {!selectedChat?.summary ? (
+                  <>
+                    <TextField
+                      label="Mesajınızı yazın"
+                      variant="outlined"
+                      value={message}
+                      onChange={handleChange}
+                      multiline
+                      minRows={1}
+                      maxRows={5}
+                      fullWidth
+                      sx={{
+                        "& .MuiInputBase-root": {
+                          minHeight: "40px",
+                        },
+                      }}
+                    />
+                    <Button
+                      disabled={loading}
+                      variant="contained"
+                      color="primary"
+                      sx={{ marginTop: 2 }}
+                      onClick={sendMessage}
+                    >
+                      {loading ? "Gönderiliyor.." : "Gönder"}
+                    </Button>
+                  </>
+                ) : (
                   <Button
-                    disabled={loading}
                     variant="contained"
                     color="primary"
                     sx={{ marginTop: 2 }}
-                    onClick={sendMessage}
+                    onClick={() => setSelectedChat(null)}
                   >
-                    {loading ? "Gönderiliyor.." : "Gönder"}
+                    Tamam
                   </Button>
-                </>
-              ) : (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{ marginTop: 2 }}
-                  onClick={() => setSelectedChat(null)}
-                >
-                  Tamam
-                </Button>
-              )}
+                )}
+              </Box>
             </Box>
-          </Box>
-          <Box mt={2} sx={{ textAlign: "center", p: 2 }}>
-            <Divider />
-            <Typography variant="body2" color="textSecondary" sx={{ mt: 1 }}>
-              © 2024 Makale Özetleyici
-            </Typography>
-          </Box>
-        </Container>
-        <Box
-          sx={{
-            display: "flex",
-            width: "30%",
-            height: "70%",
-            paddingX: "16px",
-            backgroundColor: "background.default",
-            borderRadius: "8px",
-            overflow: "scroll",
-          }}
-        >
-          {chats != undefined
-            ? chats.map((chat: any, index: number) => (
-                <ChatRow key={index} chat={chat} selectChat={selectChat} />
-              ))
-            : null}
+          </Grid>
+
+          {/* Chat List Section */}
+          <Grid item xs={12} md={4} sx={{ height: "100%" }}>
+            <Box
+              sx={{
+                width: "80%",
+                height: "90%",
+                backgroundColor: "background.default",
+                borderRadius: 1,
+                boxShadow: 3,
+                overflowY: "auto",
+                padding: 2,
+              }}
+            >
+              {chats != undefined
+                ? chats.map((chat: any, index: number) => (
+                    <ChatRow key={index} chat={chat} selectChat={selectChat} />
+                  ))
+                : null}
+            </Box>
+          </Grid>
+        </Grid>
+        <Box mt={2} sx={{ textAlign: "center", p: 2 }}>
+          <Divider />
+          <Typography variant="body2" color="textSecondary" sx={{ mt: 1 }}>
+            © 2024 Makale Özetleyici
+          </Typography>
         </Box>
       </Box>
     </ThemeProvider>
